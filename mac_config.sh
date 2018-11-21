25c0f8167ee058591830d4c18a33d48a133a5056
Will Warren <will.warren04@gmail.com>
2018-11-21 15:43:45 -0500

    refactored mac config script

--- mac_config.sh
+++ mac_config.sh
@@ -1,36 +1,65 @@
 # config script for macbook pro
 #
-### takes git name & git email as argvars
- 
-### covers basic set up
+#  Parameters:
+#	name  (string) - the name you wish to use in git
+#	email (string) - the email you wish to use in git
 #
-#
+# covers basic set up
 
+echo "Beginning Config...\n\n"
 
 # homebrew & basics
+if test ! $(which brew);
+	echo "Installing brew...\n"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# update
+brew update
 
-brew install perl@5.18
-brew cask install iterm2
-brew cask install google-ch
-brew install git
+PACKAGES=(
+	git
+	perl@5.18
+	python
+	python3
+	ruby
+)
+
+echo "Installing packages...\n"
+brew install${PACKAGES[@]}
+
+echo "Cleaning up...\n"
+brew install caskroom/cask/brew-cask
+
+CASKS=(
+	iterm2
+	google-chrome
+	firefoxdeveloperedition
+	postman
+)
+
+echo "Installing casks...\n"
+brew cask install ${CASKS[@]}
 
 # git config
 git config --global user.name  $1
 git config --global user.email $2
 
+
 # nvm
-curl -0- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+echo "Installing NVM...\n"
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
 # oh-my-zsh
+echo "Installing Oh My Zsh...\n"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ## add nvm to .zshrc
 export NVM_DIR="$HOME/.nvm" [ -s "$NVM_DIR/nvm.sh" ] && \.'$NVM_DIR/nvm.sh" >> ~/.zshrch
 
-# make workspaces
 
+# make workspaces
+echo "Making workspaces...\n"
 mkdir Sites
 mkdir Sites/Test
 touch Sites/Test/index.html
@@ -40,23 +69,27 @@ mkdir Utils
 mkdir DrawingBoard
 
 # write a few aliases
-alias gstat="git status"
-alias gchk="git checkout"
-alias gcmt="git commit -m"
-alias gadda="git add ."
-alias grst="git reset"
-alias gundo="git checkout --"
-alias gmrg="git merge"
-alias gpsh="git push"
-alias gpll="git pull"
-alias grm_local="git branch -D"
-alias grm_feature="git push :origin master"
-alias gftch="git fetch"
-alias gsqa="git squash --merge"
+ALIASES=( ["gstat"]="git status"
+	  ["gchk"]="git checkout"
+	  ["gcmt"]="git commit -m"
+   	  ["gadda"]="git add ."
+	  ["grst"]="git reset"
+	  ["gundo"]="git checkout --"
+	  ["gmrg"]="git merge"
+ 	  ["gpsh"]="git push"
+	  ["gpll"]="git pull"
+	  ["grm_local"]="git branch -D"
+	  ["grm_feature"]="git push origin master :"
+	  ["gftch"="git fetch"
+	  ["gsqa"]="git squash --merge"
+	  ["bounce"]="sudo apachectl restart"
+	  ["nano"]="sudo nano"
+)
 
-alias bouce="sudo apachectl restart"
-alias nano="sudo nano"
+echo "Writing aliases...\n"
 
+for als in "${!AlIASES[@]}";
+    	do export alias $als="${ALIASES[$als]}";
+	done
 
-# learn node 
-sudo npm install learnyounode
+echo "Mac Config Complete!"
